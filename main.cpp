de053d1a82e8a44654ac5d0e7d59e23677ba6d3a
Alexey <agafonovalexeys@gmail.com>
2018-08-06 08:39:18 -0700

    demonization added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
 #include "getopt.h"
+
 
 using namespace std;
 
@@ -9,7 +13,8 @@ int main(int argc, char **argv){
 	int opt;
 	int port=80;
 	const char* server_address = "127.0.0.1";
-	const char* directory = "/temp/server/";
+	const char* directory = "/tmp/server/";
+	pid_t daemon_pid;
 	while((opt = getopt(argc, argv, "h:p:d:")) !=-1){
 		switch(opt){
 			case 'h':
@@ -37,7 +42,32 @@ int main(int argc, char **argv){
 	cout<<"Server address: "<<server_address<<endl;
 	cout<<"Established port:"<<port<<endl;
 	cout<<"Directory: "<<directory<<endl; 
-	cout<<"test completed"<<std::endl;
+//	cout<<"test completed"<<std::endl;
+
+	daemon_pid = fork();
+	if(daemon_pid<0){
+		return -1;
+	}
+	//child process
+	else if(daemon_pid==0){
+		//new rules;
+		umask(0);
+		//new session
+		setsid();
+		//move to directory
+		chdir(directory);
+		//close IO descriptors
+		close(STDIN_FILENO);
+		close(STDOUT_FILENO);
+		close(STDERR_FILENO);
+		// open file for io
+	}
+	else{
+		return 0;
+	}
+
+	while(1);
+
 	return 0;
 }
 
